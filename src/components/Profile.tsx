263ee20d9c57e4eb49f90448304c8a8347bae1cd
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-02-23 16:24:43 -0300

    chore: Icons + completed challenge component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,18 @@
+import { Profiler } from 'react';
+
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src='https://github.com/jarlei3m.png' alt='Jarlei Rodrigues' />
+      <div>
+        <strong>Járlei Rodrigues</strong>
+        <p>
+          <img src='icons/level.svg' alt='Level' />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
