852f4b00941710203f5ecb2be46a5c1d85ffb492
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-03-08 17:14:20 -0300

    feat: Login session features

---
+++ src/context/LoginContext.tsx
@@ -0,0 +1,67 @@
+import Cookies from 'js-cookie';
+import { signIn, useSession } from 'next-auth/client';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+
+interface LoginContextData {
+  name: string;
+  image: string;
+  email: string;
+  hasGithubProfile: boolean;
+  changeLoginMethod: (action: boolean) => void;
+  handleSignIn: () => void;
+}
+
+interface LoginProviderProps {
+  children: ReactNode;
+  name: string;
+  image: string;
+  email: string;
+}
+
+export const LoginContext = createContext({} as LoginContextData);
+
+export function LoginProvider({ children, ...rest }: LoginProviderProps) {
+  const [session] = useSession();
+
+  const [name, setName] = useState(rest.name);
+  const [image, setImage] = useState(rest.image);
+  const [email, setEmail] = useState(rest.email);
+  const [hasGithubProfile, setHasGithubProfile] = useState(true);
+
+  function changeLoginMethod(action) {
+    setHasGithubProfile(action);
+  }
+
+  function handleSignIn() {
+    signIn('github', { callbackUrl: 'http://localhost:3000/home' });
+    // signIn('', { callbackUrl: 'https://appmoveit-five.vercel.app/home' });
+  }
+
+  useEffect(() => {
+    if (session) {
+      setName(session.user.name);
+      setImage(session.user.image);
+      setEmail(session.user.email);
+
+      // set cookies
+      Cookies.set('name', name);
+      Cookies.set('image', image);
+      Cookies.set('email', email);
+    }
+  }, [session]);
+
+  return (
+    <LoginContext.Provider
+      value={{
+        hasGithubProfile,
+        changeLoginMethod,
+        handleSignIn,
+        name,
+        image,
+        email,
+      }}
+    >
+      {children}
+    </LoginContext.Provider>
+  );
+}
