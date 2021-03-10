d03e80c075359ead46bf85cdf96fcc41defd7b88
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-03-10 09:41:19 -0300

    style: Responsive home/challenge page

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,16 +2,22 @@ import Cookies from 'js-cookie';
 import { signOut } from 'next-auth/client';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ChallengesContext } from '../context/ChallengesContext';
 import styles from '../styles/components/SideBar.module.css';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { FiHome } from 'react-icons/fi';
 import { GiRibbonMedal } from 'react-icons/gi';
+import { FaBars } from 'react-icons/fa';
 
 export function SideBar(): JSX.Element {
   const { route } = useRouter();
   const { refreshData } = useContext(ChallengesContext);
+  const [openSidebar, setOpenSidebar] = useState(false);
+
+  function handleOpenSideBar() {
+    setOpenSidebar(!openSidebar);
+  }
 
   if (route === '/') {
     return null;
@@ -30,27 +36,50 @@ export function SideBar(): JSX.Element {
   }
 
   return (
-    <aside className={styles.sideBarContainer}>
-      <div className={styles.sideBarHeader}>
-        <img src='/sidebar-logo.png' alt='logo' />
-      </div>
-      <div className={styles.sideBarLinks}>
-        <Link href='/home'>
-          <button className={route === '/home' ? `${styles.linkActive}` : null}>
-            <FiHome />
-          </button>
-        </Link>
-        <Link href='/leaderboard'>
-          <button
-            className={route === '/leaderboard' ? `${styles.linkActive}` : null}
-          >
-            <GiRibbonMedal />
-          </button>
-        </Link>
-      </div>
-      <div className={styles.sideBarLogout}>
-        <AiOutlineLogout onClick={() => handleSignOut()} />
-      </div>
-    </aside>
+    <>
+      <aside
+        className={
+          openSidebar
+            ? `${styles.sideBarContainer} ${styles.active}`
+            : `${styles.sideBarContainer}`
+        }
+      >
+        <div className={styles.sideBarHeader}>
+          <img onClick={handleOpenSideBar} src='/sidebar-logo.png' alt='logo' />
+        </div>
+        <div className={styles.sideBarLinks}>
+          <Link href='/home'>
+            <button
+              className={route === '/home' ? `${styles.linkActive}` : null}
+            >
+              <FiHome />
+            </button>
+          </Link>
+          <Link href='/leaderboard'>
+            <button
+              className={
+                route === '/leaderboard' ? `${styles.linkActive}` : null
+              }
+            >
+              <GiRibbonMedal />
+            </button>
+          </Link>
+        </div>
+        <div className={styles.sideBarLogout}>
+          <AiOutlineLogout onClick={() => handleSignOut()} />
+        </div>
+      </aside>
+      <aside>
+        <div
+          className={
+            openSidebar
+              ? `${styles.openSidebarBtn}`
+              : `${styles.openSidebarBtn} ${styles.show}`
+          }
+        >
+          <img onClick={handleOpenSideBar} src='/sidebar-logo.png' alt='logo' />
+        </div>
+      </aside>
+    </>
   );
 }
