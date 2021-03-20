7e37f83ebd00b695fb455d2888f9c374498badd7
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-03-20 16:35:19 -0300

    style: Added leaderboard page responsive

--- src/components/RankingBoard.tsx
+++ src/components/RankingBoard.tsx
@@ -8,24 +8,26 @@ export function RankingBoard({ properties }) {
           <article className={styles.rankingBoard}>
             <span>{index + 1}</span>
 
-            <div className={styles.rankingBoardPerfil}>
-              <img src={prop.image} alt={prop.name} />
-              <div>
-                <strong>{prop.name}</strong>
-                <p>
-                  <img src='icons/level.svg' alt='Level' />
-                  Level {prop.level}
-                </p>
+            <div className={styles.rankingBoardInfo}>
+              <div className={styles.rankingBoardPerfil}>
+                <img src={prop.image} alt={prop.name} />
+                <div>
+                  <strong>{prop.name}</strong>
+                  <p>
+                    <img src='icons/level.svg' alt='Level' />
+                    Level {prop.level}
+                  </p>
+                </div>
+              </div>
+
+              <div className={styles.rankingBoardDatas}>
+                <span>{prop.challengesCompleted}</span> completados
+              </div>
+
+              <div className={styles.rankingBoardDatas}>
+                <span>{prop.currentExperience}</span> xp
               </div>
             </div>
-
-            <p className={styles.rankingBoardDatas}>
-              <span>{prop.challengesCompleted}</span> completados
-            </p>
-
-            <p className={styles.rankingBoardDatas}>
-              <span>{prop.currentExperience}</span> xp
-            </p>
           </article>
         );
       })}
--- src/components/RankingTitles.tsx
+++ src/components/RankingTitles.tsx
@@ -2,11 +2,15 @@ import styles from '../styles/components/RankingTitles.module.css';
 
 export function RankingTitles() {
   return (
-    <div className={styles.rankingTitles}>
-      <span>Posição</span>
-      <span>Usuário</span>
-      <span>Desafios</span>
-      <span>Experiência</span>
-    </div>
+    <article className={styles.rankingTitles}>
+      <div>
+        <span>Posição</span>
+      </div>
+      <div className={styles.userInfo}>
+        <span>Usuário</span>
+        <span>Desafios</span>
+        <span>Experiência</span>
+      </div>
+    </article>
   );
 }
